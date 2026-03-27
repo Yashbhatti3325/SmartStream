b4792789f658de0e67fdc523eca64b7a5f744b4a
yash bhatti <75466002+Yashbhatti3325@users.noreply.github.com>
2026-03-27 08:22:04 +0530

    getting core knowledge of project
    adding and understanding what, how, and when to do in the project of SS(SmartStream) with the dataset.

---
+++ day3_prac.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# # Load dataset
+df = pd.read_csv("IMDB Dataset.csv")
+
+# # Show first 5 rows
+# print(df.head())
+# print(df.shape)  
+# print(df['sentiment'].value_counts())
