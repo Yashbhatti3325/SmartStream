f7e48c3b961e1abc495a17825cff32e276c17c94
yash bhatti <75466002+Yashbhatti3325@users.noreply.github.com>
2026-04-03 22:44:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# print("Hello Yash!!!")
-# print("Welcome to Python Programming") 
-
-# name = input("Please enter your name: ")
-# print("Hello " + name + "!!!")
-# print("Welcome to Python Programming, " + name + "!!!") 
-
-name = input("Enter your name: ")
-print("Hello", name)
