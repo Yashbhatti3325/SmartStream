79a83d2da10a918d4a3cb84922506d897b4162ec
yash bhatti <75466002+Yashbhatti3325@users.noreply.github.com>
2026-04-03 22:33:26 +0530

    Delete day3_prac.py

--- day3_prac.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# # Load dataset
-df = pd.read_csv("IMDB Dataset.csv")
-
-# # Show first 5 rows
-# print(df.head())
-# print(df.shape)  
-# print(df['sentiment'].value_counts())
